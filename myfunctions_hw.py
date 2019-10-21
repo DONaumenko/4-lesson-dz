737ed0f0d923adf7e587ceb82caa10c1336cc035
DONaumenko <dmitry.naumenko@gmail.com>
2019-10-22 00:33:48 +0300

    dz-4

--- myfunctions_hw.py
+++ myfunctions_hw.py
@@ -4,13 +4,11 @@ def simple_separator():
 print(simple_separator() == '**********')  # True
 
 
-
 def long_separator(count):
     return '*' * count
 
 print(long_separator(3) == '***')  # True
 print(long_separator(4) == '****')  # True
-
 
 
 def separator(simbol, count):
@@ -20,12 +18,10 @@ print(separator('-', 10) == '----------')  # True
 print(separator('#', 5) == '#####')  # True
 
 
-
 def hello_world():
     print('Hello World!')
 
 hello_world()
-
 
 
 def hello_who(who='World'):
@@ -37,19 +33,12 @@ hello_who('Max')
 hello_who('Kate')
 
 
-
 def pow_many(power, *args):
-
-#    result =
-
-    """
-    Функция складывает любое количество цифр и возводит результат в степень power (примеры использования ниже)
-    :param power: степень
-    :param args: любое количество цифр
-    :return: результат вычисления # True -> (1 + 2)**1
-    """
-    pass
-
+    result = 0
+    for item in args:
+        result += item
+    result = result ** power
+    return result
 
 print(pow_many(1, 1, 2) == 3)  # True -> (1 + 2)**1 == 3
 print(pow_many(1, 2, 3) == 5)  # True -> (2 + 3)**1 == 5
@@ -58,43 +47,18 @@ print(pow_many(3, 2) == 8)  # True -> 2**3 == 8
 print(pow_many(2, 1, 2, 3, 4) == 100)  # True -> (1 + 2 + 3 + 4)**2 == 10**2 == 100
 
 
-
 def print_key_val(**kwargs):
-    """
-    Функция выводит переданные параметры в фиде key --> value
-    key - имя параметра
-    value - значение параметра
-    :param kwargs: любое количество именованных параметров
-    :return: None
-    """
-    pass
+    for key, value in kwargs.items():
+        print(f' {key} --> {value}')
 
-
-"""
-name --> Max
-age --> 21
-"""
 print_key_val(name='Max', age=21)
-"""
-animal --> Cat
-is_animal --> True
-"""
 print_key_val(animal='Cat', is_animal=True)
 
 
-
 def my_filter(iterable, function):
-    """
-    (Усложненое задание со *)
-    Функция фильтрует последовательность iterable и возвращает новую
-    Если function от элемента последовательности возвращает True, то элемент входит в новую последовательность иначе нет
-    (примеры ниже)
-    :param iterable: входаня последовательности
-    :param function: функция фильтрации
-    :return: новая отфильтрованная последовательность
-    """
-    pass
-
+    # приводим последовательность к списку, и уже к нему применяем filter
+    iterable = list(filter(function, iterable))
+    return iterable
 
 print(my_filter([1, 2, 3, 4, 5], lambda x: x > 3) == [4, 5])  # True
 print(my_filter([1, 2, 3, 4, 5], lambda x: x == 2) == [2])  # True
